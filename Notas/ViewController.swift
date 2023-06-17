a484cda73b4c4fdf80907e5cfefee458fce4196f
Alberto Ambriz <albertpoambez@gmail.com>
2023-06-16 18:48:16 -0600

    Initial Commit

---
+++ Notas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notas
+//
+//  Created by Alberto Ambriz on 16/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
